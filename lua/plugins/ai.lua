9eaba2f670a85345ec5d58afda30f8aa7daab68d
Txz <underwoodtan@163.com>
2023-07-06 22:41:00 +0800

    add chatgpt plugin

---
+++ lua/plugins/ai.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("chatgpt").setup({
+        api_key_cmd = "echo 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"
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  },
+}
