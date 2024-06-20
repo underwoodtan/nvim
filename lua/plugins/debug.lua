7e0272c12e0dcdf5526a66ed9bc7c88cf6aaee22
txz <underwoodtan@163.com>
2024-06-20 14:21:27 +0800

    update trouble.nvim config

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -2,33 +2,41 @@ return {
   {
     "folke/trouble.nvim",
     event = "VeryLazy",
-    opts = { use_diagnostic_signs = true },
+    opts = {
+      icons = {
+        kinds = require("icons").kinds
+      },
+    },
     keys = {
-      { "<leader>xx", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics (Trouble)" },
-      { "<leader>xX", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics (Trouble)" },
-      { "<leader>xL", "<cmd>TroubleToggle loclist<cr>", desc = "Location List (Trouble)" },
-      { "<leader>xQ", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List (Trouble)" },
       {
-        "[q",
-        function()
-          if require("trouble").is_open() then
-            require("trouble").previous({ skip_groups = true, jump = true })
-          else
-            vim.cmd.cprev()
-          end
-        end,
-        desc = "Previous trouble/quickfix item",
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
       },
       {
-        "]q",
-        function()
-          if require("trouble").is_open() then
-            require("trouble").next({ skip_groups = true, jump = true })
-          else
-            vim.cmd.cnext()
-          end
-        end,
-        desc = "Next trouble/quickfix item",
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
       },
     },
   },
@@ -39,29 +47,29 @@ return {
     config = true,
     -- stylua: ignore
     keys = {
-      { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
-      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
-      { "<leader>xt", "<cmd>TodoTrouble<cr>", desc = "Todo (Trouble)" },
-      { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+      { "]t",         function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+      { "[t",         function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+      { "<leader>xt", "<cmd>TodoTrouble<cr>",                              desc = "Todo (Trouble)" },
+      { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>",      desc = "Todo/Fix/Fixme (Trouble)" },
       -- { "<leader>st", "<cmd>TodoTelescope<cr>", desc = "Todo" },
     },
   },
   {
     "rcarriga/nvim-dap-ui",
-    dependencies = {"mfussenegger/nvim-dap", "nvim-neotest/nvim-nio"},
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
     opts = {},
     keys = {
-      { "<leader>du", function () require("dapui").toggle() end, desc = "Toggle Dap UI" },
-      { "<leader>dv", function () require("dapui").eval() end, desc = "Toggle Dap UI" },
-      { "<leader>dwa", function () require("dapui").elements.watches.add() end, desc = "Toggle Watch Add" },
-      { "<leader>dwd", function () require("dapui").elements.watches.remove() end, desc = "Toggle Watch Delate" },
+      { "<leader>du",  function() require("dapui").toggle() end,                  desc = "Toggle Dap UI" },
+      { "<leader>dv",  function() require("dapui").eval() end,                    desc = "Toggle Dap UI" },
+      { "<leader>dwa", function() require("dapui").elements.watches.add() end,    desc = "Toggle Watch Add" },
+      { "<leader>dwd", function() require("dapui").elements.watches.remove() end, desc = "Toggle Watch Delate" },
     }
   },
   {
     "mfussenegger/nvim-dap",
     keys = {
       { "<leader>db", "<cmd>DapToggleBreakpoint<CR>", desc = "Toggle Dap Breakpoint" },
-      { "<leader>dc", "<cmd>DapContinue<CR>", desc = "Dap Continue" },
+      { "<leader>dc", "<cmd>DapContinue<CR>",         desc = "Dap Continue" },
     }
   }
 }
